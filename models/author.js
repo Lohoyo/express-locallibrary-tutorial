bc0798ce2df5203a8fe861c57a2c99be4be2e376
liuhuyue <liuhuyue@baidu.com>
2019-03-10 16:31:23 +0800

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,50 @@
+var moment = require('moment');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: { type: String, required: true, max: 100 },
+    family_name: { type: String, required: true, max: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+  .virtual('name')
+  .get(function () {
+    return this.family_name + ', ' + this.first_name;
+  });
+
+// Virtual for author's URL
+AuthorSchema
+  .virtual('url')
+  .get(function () {
+    return '/catalog/author/' + this._id;
+  });
+
+// AuthorSchema
+//   .virtual('date_of_birth_formatted')
+//   .get(function () {
+//     return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+//   });
+
+// AuthorSchema
+//   .virtual('date_of_death_formatted')
+//   .get(function () {
+//     return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+//   });
+
+AuthorSchema
+  .virtual('lifespan')
+  .get(function () {
+    var date_of_birth_formatted = this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '';
+    var date_of_death_formatted = this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+    return (date_of_birth_formatted || date_of_death_formatted) ? (date_of_birth_formatted + ' - ' + date_of_death_formatted) : '';
+  });
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
